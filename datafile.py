998072f97ef1105eeeb16bc23996ec9f4fc8acf5
Nilyang404 <63556313+Nilyang404@users.noreply.github.com>
2020-11-21 12:18:09 +0800

    引入训练数据集Caltech-UCSD Birds-200-2011

---
+++ datafile.py
@@ -0,0 +1,13 @@
+#using utf-8
+'''
+数据集说明:
+Caltech-UCSD Birds-200-2011
+官方网站:http://www.vision.caltech.edu/visipedia/CUB-200-2011.html
+（1）类别数目: 200
+（2）图像总数目: 11,788
+（3）每张图片的标注信息: 15 Part Locations, 312 Binary Attributes, 1 Bounding Box
+
+'''
+imort unzp
+!wget http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz
+
